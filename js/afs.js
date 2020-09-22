ff6ec7383bcf249229216aef839c23ee94e68157
gentsao <61525709+gentsaot@users.noreply.github.com>
2020-09-22 15:49:16 -0400

    Built Responsive Nav
    There are a couple kinks to work out, but this should be functional. It's on each page now.

---
+++ js/afs.js
@@ -0,0 +1,20 @@
+console.log('jQuery is working woohoo!');
+
+(function ($) {
+    $(function () {
+        $('nav ul li > a:not(:only-child)').click(function (e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            $('.nav-dropdown').not($(this).siblings()).hide();
+            e.stopPropagation()
+        });
+        $('html').click(function () {
+            $('.nav-dropdown').hide();
+        });
+        $('#nav-toggle').on('click', function () {
+           this.classList.toggle('active') 
+        });
+        $('#nav-toggle').click(function () {
+            $('nav ul').toggle()
+        });
+    })
+  })(jQuery);
